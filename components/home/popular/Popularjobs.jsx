fa089d121b09d575af88463e5cd6e5db4f07c853
LuisBrodely <luisbrodely2@gmail.com>
2023-08-11 12:37:49 -0600

    parte 1/2 finalizada

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -25,6 +25,13 @@ const Popularjobs = () => {
       num_pages: '1'
     }
   )
+
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -46,6 +53,8 @@ const Popularjobs = () => {
             renderItem={({item}) => (
               <PopularJobCard
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={item => item?.job_id}
