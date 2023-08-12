a7d838e6eb270661e0316ca53e69189d5150635c
LuisBrodely <luisbrodely2@gmail.com>
2023-08-11 23:50:06 -0600

    Actualizando

---
+++ app/job-details/[id].js
@@ -0,0 +1,81 @@
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { SIZES, COLORS, icons } from "../../constants";
+import useFetch from "../../hook/useFetch";
+
+const JobsDetails = () => {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch("search", {
+    job_id: params.id,
+  });
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {};
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.share} dimension="60%" />
+          ),
+          headerTitle: "",
+        }}
+      >
+        <>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+          >
+            {isLoading ? (
+              <ActivityIndicator size="large" color={COLORS.primary} />
+            ) : error ? (
+              <Text>Something went wrong</Text>
+            ) : data.lenght === 0 ? (
+              <Text>No data</Text>
+            ) : (
+              <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+                <Company />
+                <JobTabs />
+              </View>
+            )}
+          </ScrollView>
+        </>
+      </Stack.Screen>
+    </SafeAreaView>
+  );
+};
+
+export default JobsDetails;
